a57694b0c9c86e33befd44d096922f7b568e291e
Benjamin McFarland <benrmcfarland@gmail.com>
2020-11-03 13:12:06 -0800

    Add code for buisness logic to make contacts and delete them
    Co-authored-by: Tyler Sinks <tyler.sinks@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,51 @@
 $(document).ready(function() {
-
+  // User Interface Logic here:
+  // dummy userNames, etc... I like it
 });
+
+
+// Business Logic
+function Contact(firstName, lastName, phoneMain, address) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.phoneMain = phoneMain;
+  this.address = address;
+}
+
+function ContactBook() {
+  // initialize an empty array to hold each NEW instance of a Contact
+  this.contacts = [];
+  this.currentID = 0;
+}
+
+ContactBook.prototype.addNewContact = function(contact) {
+  contact.id = this.assignId();
+  this.contacts.push(contact);
+}
+
+ContactBook.prototype.assignId = function() {
+  this.currentID++;
+  return this.currentID;
+}
+
+ContactBook.prototype.removeContact = function(id) {
+  let i = this.findContact(id);
+  if (i === false) {
+    return false;
+  } 
+  else {
+    delete this.contacts[i];
+    return true;
+  }
+}
+
+ContactBook.prototype.findContact = function(id) {
+  for (let i = 0; i < this.contacts.length; i++) {
+    if (this.contacts[i]) {
+      if (this.contacts[i].id == id) {
+        return i;
+      }
+    }
+  };
+  return false;
+}
